b127660cb73e9949c73af82751e3e30afabdda9f
CeaseFireMoments <contactceasefire@gmail.com>
2025-11-16 12:17:35 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -571,6 +571,5 @@ module.exports = function (eleventyConfig) {
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: false,
     passthroughFileCopy: true,
-    useIncremental: true,
   };
 };
